1ab727b7c9731f69949e17f54f884132628a2bcb
The Apex Typhon <b.butler31.bb@gmail.com>
2021-08-26 13:51:56 +0100

    CP updated to work with ranks

--- garrysmod/gamemodes/ixhl2rp/schema/factions/sh_metropolice.lua
+++ garrysmod/gamemodes/ixhl2rp/schema/factions/sh_metropolice.lua
@@ -3,22 +3,19 @@ FACTION.name = "Civil Protection"
 FACTION.description = "Civil Protection units employed by an unkown larger group know as MPF."
 FACTION.color = Color(50, 100, 150)
 FACTION.pay = 10
-FACTION.models = {"models/police.mdl"}
+FACTION.models = {"models/dpfilms/metropolice/hdpolice.mdl"}
 FACTION.weapons = {"ix_stunstick"}
 FACTION.isDefault = false
-FACTION.isGloballyRecognized = true
 FACTION.runSounds = {[0] = "NPC_MetroPolice.RunFootstepLeft", [1] = "NPC_MetroPolice.RunFootstepRight"}
 
 function FACTION:OnCharacterCreated(client, character)
 	local inventory = character:GetInventory()
-
-	inventory:Add("pistol", 1)
+	
 	inventory:Add("flashlight", 1)
-	inventory:Add("pistolammo", 2)
 end
 
 function FACTION:GetDefaultName(client)
-	return "C2.i4-" .. Schema:ZeroNumber(math.random(1, 99999), 5), true
+	return "C2.i4-" .. Schema:ZeroNumber(math.random(1, 9999), 4), true
 end
 
 function FACTION:OnTransfered(client)
@@ -32,9 +29,15 @@ function FACTION:OnNameChanged(client, oldValue, value)
 	local character = client:GetCharacter()
 
 	if (!Schema:IsCombineRank(oldValue, "i4") and Schema:IsCombineRank(value, "i4")) then
-		character:JoinClass(CLASS_MPR)
+		character:JoinClass(CLASS_I4)
+	elseif (!Schema:IsCombineRank(oldValue, "i3") and Schema:IsCombineRank(value, "i3")) then
+		character:JoinClass(CLASS_I3)
+	elseif (!Schema:IsCombineRank(oldValue, "i2") and Schema:IsCombineRank(value, "i2")) then
+		character:JoinClass(CLASS_I3)
+	elseif (!Schema:IsCombineRank(oldValue, "i1") and Schema:IsCombineRank(value, "i1")) then
+		character:JoinClass(CLASS_I3)
 	elseif (!Schema:IsCombineRank(oldValue, "OfC") and Schema:IsCombineRank(value, "OfC")) then
-		character:SetModel("models/policetrench.mdl")
+		
 	elseif (!Schema:IsCombineRank(oldValue, "SqL") and Schema:IsCombineRank(value, "SqL")) then
 		character:JoinClass(CLASS_EMP)
 
