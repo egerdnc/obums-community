6e6596f5746584d5b510b8b91df449857908e952
Ege Göktuğ Erdinç <ege.erdinc23@gmail.com>
2021-08-26 08:02:30 +0300

    Sexy stuff?

---
+++ garrysmod/gamemodes/ixhl2rp/schema/items/armor_clothes/sh_ota_vest.lua
@@ -0,0 +1,37 @@
+ITEM.name = "Regular Overwatch Vest"
+ITEM.description = "A vest taken from one of The Overwatch Soldiers."
+ITEM.model = Model("models/combine_vests/elitevest.mdl")
+ITEM.category = "Vest"
+ITEM.width = 2
+ITEM.height = 2
+ITEM.maxArmor = 70
+
+if (CLIENT) then
+	function ITEM:PopulateTooltip(tooltip)
+		local panel = tooltip:AddRowAfter("name", "armor")
+		panel:SetBackgroundColor(derma.GetColor("Warning", tooltip))
+		panel:SetText("Armor: " .. (self:GetData("equip") and LocalPlayer():Armor() or self:GetData("armor", self.maxArmor)))
+		panel:SizeToContents()
+	end
+end
+
+function ITEM:OnEquipped()
+	self.player:SetArmor(self:GetData("armor", self.maxArmor))
+end
+
+function ITEM:OnUnequipped()
+	self:SetData("armor", math.Clamp(self.player:Armor(), 0, self.maxArmor))
+	self.player:SetArmor(0)
+end
+
+function ITEM:OnLoadout()
+	if (self:GetData("equip")) then
+		self.player:SetArmor(self:GetData("armor", self.maxArmor))
+	end
+end
+
+function ITEM:OnSave()
+	if (self:GetData("equip")) then
+		self:SetData("armor", math.Clamp(self.player:Armor(), 0, self.maxArmor))
+	end
+end
