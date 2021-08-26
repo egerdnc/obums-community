c225b94552e3ebb6f262a10df9fe3ca1282bf160
Ege Göktuğ Erdinç <ege.erdinc23@gmail.com>
2021-08-27 01:50:42 +0300

    /ad mug 15k tokens or die + XP

---
+++ garrysmod/gamemodes/ixhl2rp/plugins/xp/sh_language.lua
@@ -0,0 +1,7 @@
+local plugin = PLUGIN
+
+ix.lang.AddTable("english", {
+	xpGainTime = "You got %s XP for playing in the server!",
+    xpAborted = "Your XP has been reset!",
+    xpSet = "Your XP was set on %s"
+})
