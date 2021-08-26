9e1b21242bca2ec36859f77ce83ace54613ce86e
Ege Göktuğ Erdinç <ege.erdinc23@gmail.com>
2021-08-26 07:49:39 +0300

    Being worked on: OTA Vests

---
+++ garrysmod/gamemodes/ixhl2rp/schema/items/armor_clothes/sh_elite_vest.lua
@@ -0,0 +1,41 @@
+ITEM.name = "Elite Overwatch Vest"
+ITEM.description = "A vest taken from one of The Elite Overwatch Soldiers."
+ITEM.model = Model("models/combine_vests/elitevest.mdl")
+ITEM.category = "Armored Clothing"
+ITEM.width = 2
+ITEM.height = 2
+ITEM.maxArmor = 100
+
+if (CLIENT) then
+	function ITEM:PopulateTooltip(tooltip)
+		local panel = tooltip:AddRowAfter("name", "armor")
+		panel:SetBackgroundColor(derma.GetColor("Warning", tooltip))
+		panel:SetText("Armor: " .. (self:GetData("equip") and LocalPlayer():Armor() or self:GetData("armor", self.maxArmor)))
+		panel:SizeToContents()
+	end
+end
+
+function ITEM:OnEquipped()
+	self.player:SetArmor(self:GetData("armor", self.maxArmor))
+end
+
+function ITEM:OnUnequipped()
+	self:SetData("armor", math.Clamp(self.player:Armor(), 0, self.maxArmor))
+	self.player:SetArmor(0)
+end
+
+function ITEM:Repair(amount)
+	self:SetData("armor", math.Clamp(self:GetData("armor") + amount, 0, self.maxArmor))
+end
+
+function ITEM:OnLoadout()
+	if (self:GetData("equip")) then
+		self.player:SetArmor(self:GetData("armor", self.maxArmor))
+	end
+end
+
+function ITEM:OnSave()
+	if (self:GetData("equip")) then
+		self:SetData("armor", math.Clamp(self.player:Armor(), 0, self.maxArmor))
+	end
+end
