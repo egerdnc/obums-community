6aa8e500a1e3798accd6cffd9ffa8640aafb7c1b
DutyServeUnion <84892309+DutyServeUnion@users.noreply.github.com>
2021-08-27 16:51:42 +0400

    waypoint thing

--- garrysmod/gamemodes/ixhl2rp/schema/factions/sh_metropolice.lua
+++ garrysmod/gamemodes/ixhl2rp/schema/factions/sh_metropolice.lua
@@ -4,6 +4,10 @@ FACTION.color = Color(50, 100, 150)
 FACTION.models = {"models/dpfilms/metropolice/hdpolice.mdl"}
 FACTION.isDefault = false
 FACTION.runSounds = {[0] = "NPC_MetroPolice.RunFootstepLeft", [1] = "NPC_MetroPolice.RunFootstepRight"}
+  - FACTION.canSeeWaypoints = true
+  - FACTION.canAddWaypoints = true
+  - FACTION.canRemoveWaypoints = true
+  - FACTION.canUpdateWaypoints = true
 
 function FACTION:OnCharacterCreated(client, character)
 	local inventory = character:GetInventory()
