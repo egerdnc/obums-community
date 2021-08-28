854b8962903c099d168d64c03c5c77d39e5fb283
The Apex Typhon <b.butler31.bb@gmail.com>
2021-08-28 01:05:00 +0100

    New items.

---
+++ garrysmod/gamemodes/ixhl2rp/schema/items/weapons/sh_amr11.lua
@@ -0,0 +1,13 @@
+ITEM.name = "AMR-11"
+ITEM.description = "A high powered combine-modified sniper which blows flesh and walls apart."
+ITEM.model = "models/weapons/w_amr11.mdl"
+ITEM.class = "ls_csniper"
+ITEM.weaponCategory = "primary"
+ITEM.width = 4
+ITEM.weight = 10
+ITEM.height = 2
+ITEM.iconCam = {
+	ang	= Angle(-0.020070368424058, 270.40155029297, 0),
+	fov	= 7.2253324508038,
+	pos	= Vector(0, 200, -1)
+}
---
+++ garrysmod/gamemodes/ixhl2rp/schema/items/weapons/sh_m24.lua
@@ -0,0 +1,13 @@
+ITEM.name = "M24"
+ITEM.description = "A weapon of rebel-craft, this is their equivilent to the AMR."
+ITEM.model = "models/weapons/smc/m24/w_m24.mdl"
+ITEM.class = "ls_m24"
+ITEM.weaponCategory = "primary"
+ITEM.width = 4
+ITEM.weight = 5
+ITEM.height = 2
+ITEM.iconCam = {
+	ang	= Angle(-0.020070368424058, 270.40155029297, 0),
+	fov	= 7.2253324508038,
+	pos	= Vector(0, 200, -1)
+}
