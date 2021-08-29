5efa07c67a479b104c076dab481ffd8d80ba1774
Ege Göktuğ Erdinç <ege.erdinc23@gmail.com>
2021-08-30 02:44:55 +0300

    Revert "nhn"
    This reverts commit daf783fedd24a7b3420b745106940cdf7e506fc7.

---
+++ garrysmod/gamemodes/ixhl2rp/schema/items/sh_water_sparkling.lua
@@ -0,0 +1,21 @@
+ITEM.id = "sparkling_water"
+ITEM.name = "Sparkling Breen's Water"
+ITEM.model = Model("models/props_junk/popcan01a.mdl")
+ITEM.weight = 0.1
+ITEM.skin = 1
+ITEM.description = "A red aluminium can of carbonated water."
+ITEM.category = "Consumables"
+ITEM.illegal = false
+
+ITEM.functions.Drink = {
+	OnRun = function(itemTable)
+		local client = itemTable.player
+
+		client:RestoreStamina(50)
+		client:SetHealth(math.Clamp(client:Health() + 8, 0, client:GetMaxHealth()))
+		client:EmitSound("npc/barnacle/barnacle_gulp2.wav", 75, 90, 0.35)
+	end,
+	OnCanRun = function(itemTable)
+		return !itemTable.player:IsCombine()
+	end
+}
