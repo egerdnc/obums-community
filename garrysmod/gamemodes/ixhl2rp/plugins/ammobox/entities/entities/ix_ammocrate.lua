38085aa8aef69fe188134ecee0bb4f7ee653a68e
Ege Göktuğ Erdinç <ege.erdinc23@gmail.com>
2021-08-29 05:40:04 +0300

    Ammo crate update

--- garrysmod/gamemodes/ixhl2rp/plugins/ammobox/entities/entities/ix_ammocrate.lua
+++ garrysmod/gamemodes/ixhl2rp/plugins/ammobox/entities/entities/ix_ammocrate.lua
@@ -11,9 +11,12 @@ if SERVER then
     end
 
     function ENT:Use(client)
+        self:NextThink( CurTime() + 1 )
+        if not client:IsCombine() then client:Notify("You need to be a combine unit to do this.") return end
         self:SetUseType(SIMPLE_USE)
         self:EmitSound("items/ammocrate_open.wav")
         self:SetSequence(1)
+        self:EmitSound("items/ammo_pickup.wav")
             for i, v in pairs(client:GetWeapons()) do
                 local weapon = v
                 local ammoID = weapon:GetPrimaryAmmoType()
