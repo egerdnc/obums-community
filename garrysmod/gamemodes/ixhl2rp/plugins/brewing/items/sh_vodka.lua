2fed2629fc4ee8c9ef7dd7f46e606b1738c03456
DutyServeUnion <84892309+DutyServeUnion@users.noreply.github.com>
2021-08-29 19:56:49 +0400

    Brewing system (Sydres pls help)

---
+++ garrysmod/gamemodes/ixhl2rp/plugins/brewing/items/sh_vodka.lua
@@ -0,0 +1,7 @@
+ITEM.name = "Vodka";
+ITEM.model = Model("models/props_junk/garbage_glassbottle001a.mdl");
+ITEM.width = 1;
+ITEM.height = 1;
+ITEM.description = "Be careful, your pet russian might want some.";
+ITEM.category = "Brewing";
+ITEM.noBusiness = true
