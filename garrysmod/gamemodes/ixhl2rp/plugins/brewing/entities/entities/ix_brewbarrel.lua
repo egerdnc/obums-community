0d8d1a6c000bd8b92748f6f676589330a185a550
Ege Göktuğ Erdinç <ege.erdinc23@gmail.com>
2021-08-30 02:45:00 +0300

    Revert "no"
    This reverts commit e516f1bc52557078a4b7c2e353d1b2ea6b3f9012.

---
+++ garrysmod/gamemodes/ixhl2rp/plugins/brewing/entities/entities/ix_brewbarrel.lua
@@ -0,0 +1,94 @@
+AddCSLuaFile()
+
+local PLUGIN = PLUGIN;
+
+ENT.Base = "base_entity";
+ENT.Type = "anim";
+ENT.PrintName = "Brewing Barrel";
+ENT.Category = "Helix";
+ENT.Spawnable = true;
+ENT.RenderGroup = RENDERGROUP_BOTH;
+
+local comboRisk = {
+  ["water"] = 0,
+  ["ginsparklingwater"] = 2,
+  ["ginspecialwater"] = 4,
+  ["vodkawater"] = 6,
+  ["vodkasparklingwater"] = 8,
+  ["vodkaspecialwater"] = 10,
+  ["whiskeywater"] = 15,
+  ["whiskeysparklingwater"] = 20,
+  ["whiskeyspecialwater"] = 30
+};
+
+if SERVER then
+  function ENT:Initialize()
+    self:SetModel("models/props/de_inferno/wine_barrel.mdl");
+    self:SetSolid(SOLID_VPHYSICS);
+    self:PhysicsInit(SOLID_VPHYSICS);
+
+    local physObj = self:GetPhysicsObject()
+
+    if (IsValid(physObj)) then
+      physObj:EnableMotion(true);
+      physObj:Wake();
+    end;
+
+    print(physObj:IsMoveable());
+
+    self:SetStatus("Idle");
+  end;
+
+  local callbacks = {
+    ["Idle"] = function(ent, ply)
+      net.Start("ixBrewingMenu");
+        net.WriteEntity(ent);
+      net.Send(ply);
+    end,
+    ["Brewing"] = function(ent, ply)
+      ply:Notify("This barrel is still brewing, please wait");
+    end,
+    ["Finished"] = function(ent, ply)
+      local inv = ply:GetCharacter():GetInventory();
+      local combo = ent:GetCombo();
+
+      if !PLUGIN.IngCombinations[combo] then return end;
+
+      if math.random(1, 100) <= comboRisk[combo] then -- This should work?
+        ent:EmitSound("weapons/explode5.wav");
+        ent:Remove();
+        ply:TakeDamage(35);
+      else
+        inv:Add("moonshine", 1, {quality = PLUGIN.IngCombinations[combo]()});
+        ply:Notify("You have collected the moonshine");
+        ent:SetStatus("Idle");
+        ent:SetCombo("");
+      end;
+    end;
+  };
+
+  function ENT:Use(act)
+    self:SetUseType(SIMPLE_USE);
+    local bSucceed, result = pcall(callbacks[self:GetStatus()], self, act);
+    if !bSucceed then
+      ErrorNoHalt("Warning callback for " .. self:GetStatus() .. " has failed\n " .. result);
+    end;
+  end;
+end;
+
+
+function ENT:SetupDataTables()
+  self:NetworkVar("String", 0, "Status");
+  self:NetworkVar("String", 1, "Combo");
+end;
+
+function ENT:OnRemove()
+  self:StopSound("ambient/water/underwater.wav");
+end;
+
+
+if CLIENT then
+  function ENT:Draw()
+    self:DrawModel();
+  end;
+end;
